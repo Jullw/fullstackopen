42670f52dfd98a71926d6d5294b286ac1bda6a25
Jullw <jullw@gmail.com>
2021-02-17 00:21:32 +0200

    Puhelinluettelon ensimmainen osuus

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,103 @@
+import React, {useState} from 'react'
+
+
+const Filter = ({searchName, handleSearch}) =>{
+  return(
+    <div>
+    search: <input value={searchName} onChange={handleSearch}></input>
+    </div>
+  )
+}
+
+const Add =({addPerson,newNumber,newName,handleNameChange,handleNumberChange})=>{
+
+  return(
+  <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange}/>
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange}/>
+      </div>
+      <div>
+        <button type="submit"> add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons =({personsToShow}) =>{
+  return(
+  <div>
+    {personsToShow.map((person, i) => 
+      <p key={person.name, i}>{person.name} {person.number}</p>
+      )}
+  </div>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    {name: 'Matti Leppälä', number: '+3584456739'}
+  ])
+  
+  const [ newName, setNewName] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
+  const [ searchName, setSearchName] = useState('')
+
+
+
+  const addPerson = (event) =>{
+    event.preventDefault()
+    console.log(event.target)
+
+    const newPerson = {
+      name: newName,
+      number: newNumber
+    }
+    
+    persons.some(person => person.name === newName) ?
+    window.alert(`${newName} is already added to phonebook`): 
+    setPersons(persons.concat(newPerson))
+
+    setNewName('')
+    setNewNumber('')
+    
+  } 
+
+  const handleNameChange = (event) =>{
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) =>{
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearch = (event) =>{
+    setSearchName(event.target.value)
+
+  }
+
+  const personsToShow = false
+  ? persons
+  : persons.filter(person => person.name.toLowerCase().includes(searchName))
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter searchName={searchName} handleSearch={handleSearch} />
+
+      <h2>Add</h2>
+      <Add addPerson={addPerson} newName={newName} newNumber={newNumber}
+      handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons personsToShow={personsToShow}/>
+    </div>
+  )
+}
+
+export default App;
