02092008204276c381f546d3218500f5d86724dd
Jullw <jullw@gmail.com>
2021-07-21 20:20:29 +0300

    Build FullStack to heroku

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -7,6 +7,7 @@ app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors())
+app.use(express.static('build'))
 
 
 let persons = [
@@ -28,11 +29,6 @@ let persons = [
 ]
 
 
-
-app.get('/', (req, res) => {
-    res.send('<h1> Starting site </h1>')
-
-})
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
