7772dc8e0b47976b47711b3e1b01839aba026ef2
Jullw <jullw@gmail.com>
2021-07-22 14:25:53 +0300

    3.1 - 3.18 tehtävät valmiita

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -41,17 +41,21 @@ app.get('/api/persons', (req, res) => {
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
-      response.json(persons)
+        response.json(persons)
     })
-  })
-  
+})
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person =>{
-        response.json(person)
-    })
- 
- 
+
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).send('error 404 not found').end()
+            }
+        })
+        .catch(error => next(error))
     /*   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
 
@@ -62,28 +66,37 @@ app.get('/api/persons/:id', (request, response) => {
     }  */
 })
 
-app.get('/info', (req, res) => {
-    const infoText = `<p> Phonebook has info for ${persons.length}</p> 
-    <p> ${new Date()} </p>`;
-    res.send(infoText)
+app.get('/info', (request, response) => {
+    Person.find({}).then(persons => {
+        const infoText = `<p> Phonebook has info for ${persons.length}</p> 
+        <p> ${new Date()} </p>`;
+        response.send(infoText)
+    }).catch(error => next(error))
 })
 
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
+app.delete('/api/persons/:id', (request, response, next) => {
+   /* const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+    response.status(204).end() */
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+
 })
 
+/*
 const generateID = () => {
     const randomID = Math.floor(Math.random() * 1000000) + 1;
     return randomID;
 }
-
+*/
 app.post('/api/persons', (request, response) => {
 
     const body = request.body
-    
+
     if (!body.name) {
         return response.status(400).json({
             error: "Name missing"
@@ -111,12 +124,39 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      name : body.name,
+      number : body.number
+    }
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
+}
+
 app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id!!' })
+    }
+
+    next(error)
+}
+
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
