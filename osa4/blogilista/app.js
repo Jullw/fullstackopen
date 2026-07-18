b248aa194e35c08cfdfc3a525849889880c7a9dd
Jullw <j.sajaniemi@gmail.com>
2026-07-18 21:50:47 +0300

    added login route

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const middleware = require("./utils/middleware");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const { connectToDb } = require("./utils/db_connect");
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 
