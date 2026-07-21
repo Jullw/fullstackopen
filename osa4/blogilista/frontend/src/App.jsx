0ad10e9ed04f8cb94bd469123ea2323497f7f8bb
Jullw <j.sajaniemi@gmail.com>
2026-07-21 16:08:53 +0300

    Created login component wip.

--- osa4/blogilista/frontend/src/App.jsx
+++ osa4/blogilista/frontend/src/App.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import Blogs from "./blogs/Blogs";
+import Login from "./login/Login";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
+      <Login
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+      />
       <Blogs />
     </>
   );
---
+++ osa4/blogilista/frontend/src/login/Login.jsx
@@ -0,0 +1,36 @@
+function Login({ username, setUsername, password, setPassword }) {
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log("logging in with", username, password);
+  };
+  return (
+    <>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
